6f6bfc3fabf233b59274e4b1d2eaf271ffca48ce
nicolacalvio <71388636+nicolacalvio@users.noreply.github.com>
2020-11-10 17:46:18 +0100

    aggiunta view extra

--- app/src/main/java/com/veneto_valley/veneto_valley/extra.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/extra.java
@@ -3,13 +3,15 @@ package com.veneto_valley.veneto_valley;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class extra extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_extra);
+
+
     }
 }
