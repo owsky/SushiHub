af6a65e84e543b997aadf821612e11f13bd00564
nicolacalvio <71388636+nicolacalvio@users.noreply.github.com>
2020-11-10 17:51:34 +0100

    risoluzione conflitti e set pagina iniziale

--- app/src/main/java/com/veneto_valley/veneto_valley/MainActivity.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_extra);
+        setContentView(R.layout.activity_page_iniziale);
     }
 }
