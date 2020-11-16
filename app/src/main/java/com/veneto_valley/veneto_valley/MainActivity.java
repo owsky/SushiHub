416f8a2e529a9efe1324bc561be50a7e88b6f48b
Nicolò Bertocco <nick.bertocco@gmail.com>
2020-11-16 19:18:48 +0100

    Refactoring

--- app/src/main/java/com/veneto_valley/veneto_valley/MainActivity.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page_iniziale);
+        setContentView(R.layout.activity_homepage);
 
 
     }
