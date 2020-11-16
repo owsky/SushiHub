a2241d134f3f9e674670c8b00381d14c31aae669
Nicolò Bertocco <nick.bertocco@gmail.com>
2020-11-16 19:10:29 +0100

    Refactored pagina iniziale into main activity

--- app/src/main/java/com/veneto_valley/veneto_valley/MainActivity.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/MainActivity.java
@@ -5,11 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_page_iniziale);
+
+
     }
 }
