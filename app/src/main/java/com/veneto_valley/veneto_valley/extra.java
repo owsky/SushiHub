a841dcbbb5de4dc1a510bd33229f3aa7e26142d3
Nicolò Bertocco <nick.bertocco@gmail.com>
2020-11-16 19:10:39 +0100

    Refactored extra caps

--- app/src/main/java/com/veneto_valley/veneto_valley/extra.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/extra.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class extra extends AppCompatActivity {
+public class Extra extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
