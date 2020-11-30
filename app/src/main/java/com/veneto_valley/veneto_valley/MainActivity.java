cd2dd9311d87e3cc95f5d354c12617791eeeea9a
Nicolò Bertocco <nick.bertocco@gmail.com>
2020-11-30 02:35:56 +0100

    Creato da zero la homepage, implementato lo speed dial tramite libreria di terze parti

--- app/src/main/java/com/veneto_valley/veneto_valley/MainActivity.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/MainActivity.java
@@ -1,8 +1,17 @@
 package com.veneto_valley.veneto_valley;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.veneto_valley.veneto_valley.qr.GeneraQR;
+import com.veneto_valley.veneto_valley.qr.ScanQR;
 
 public class MainActivity extends AppCompatActivity {
     
@@ -10,7 +19,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-
-
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setTitle("Homepage");
+        
+    }
+    
+    public void generaTavolo(View view) {
+        Intent intent = new Intent(MainActivity.this, GeneraQR.class);
+        startActivity(intent);
+    }
+    
+    public void unisciTavolo(View view) {
+        Intent intent = new Intent(MainActivity.this, ScanQR.class);
+        startActivity(intent);
     }
 }
