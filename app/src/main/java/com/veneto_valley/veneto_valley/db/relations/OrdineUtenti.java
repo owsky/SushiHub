5a33e78bc59fc8526dd605fa346182eb8b2dd5fa
MBP Alessandro <aleben98@gmail.com>
2020-12-10 17:55:36 +0100

    Room - Implementazione Relazioni

---
+++ app/src/main/java/com/veneto_valley/veneto_valley/db/relations/OrdineUtenti.java
@@ -0,0 +1,21 @@
+package com.veneto_valley.veneto_valley.db.relations;
+
+import androidx.room.Embedded;
+import androidx.room.Junction;
+import androidx.room.Relation;
+
+import com.veneto_valley.veneto_valley.db.entities.Ordine;
+import com.veneto_valley.veneto_valley.db.entities.Utente;
+
+import java.util.List;
+
+public class OrdineUtenti {
+    @Embedded
+    public Ordine ordine;
+    @Relation(
+            parentColumn = "idOrdine",
+            entityColumn = "idUtente",
+            associateBy = @Junction(UtentiOrdiniCrossRef.class)
+    )
+    public List<Utente> utenti;
+}
