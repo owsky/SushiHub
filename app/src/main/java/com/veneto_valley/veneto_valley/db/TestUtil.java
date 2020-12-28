26429ac0fdbb37adf08b7003bb813e98bc02a918
MBP Alessandro <aleben98@gmail.com>
2020-12-28 22:00:14 +0100

    Room - basic tavolo test

--- app/src/main/java/com/veneto_valley/veneto_valley/db/TestUtil.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/db/TestUtil.java
@@ -1,4 +1,19 @@
 package com.veneto_valley.veneto_valley.db;
 
+import com.veneto_valley.veneto_valley.db.entities.Tavolo;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class TestUtil {
+    public static ArrayList<Tavolo> creaTavoli(int nTavoli){
+        ArrayList<Tavolo> tavoloArrayList = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i<nTavoli;i++){
+            tavoloArrayList.add(new Tavolo(Integer.toString(rand.nextInt(1000))+(char)(rand.nextInt(25)+65),rand.nextInt(10),rand.nextFloat()*10));
+        }
+
+        return tavoloArrayList;
+    }
 }
--- app/src/main/java/com/veneto_valley/veneto_valley/db/entities/Tavolo.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/db/entities/Tavolo.java
@@ -2,6 +2,7 @@ package com.veneto_valley.veneto_valley.db.entities;
 
 import android.util.Log;
 
+import androidx.annotation.Nullable;
 import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
@@ -28,7 +29,6 @@ public class Tavolo {
 
     public int ristorante;
 
-
     @Ignore
     public Tavolo(String idTavolo, int maxPiatti, float costoMenu) {
         this.idTavolo = idTavolo;
@@ -38,6 +38,7 @@ public class Tavolo {
         Log.d(TAG,"Current time => " + this.dataCreazione);
     }
 
+    @Ignore
     public Tavolo(String idTavolo, String nomeTavolo, int maxPiatti, float costoMenu) {
        this(idTavolo, maxPiatti, costoMenu);
        this.nome = nomeTavolo;
@@ -46,5 +47,25 @@ public class Tavolo {
     public Tavolo(String idTavolo, int maxPiatti, float costoMenu, int ristorante) {
         this(idTavolo, maxPiatti, costoMenu);
         this.ristorante = ristorante;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        // If the object is compared with itself then return true
+        if (obj == this) {
+            return true;
+        }
+
+        /* Check if o is an instance of Complex or not
+          "null instanceof [type]" also returns false */
+        if (!(obj instanceof Tavolo)) {
+            return false;
+        }
+
+        // typecast o to Complex so that we can compare data members
+        Tavolo t = (Tavolo) obj;
+
+        // Compare the data members and return accordingly
+        return this.idTavolo.equals(t.idTavolo); //TODO: Ampliare Confronto
     }
 }
