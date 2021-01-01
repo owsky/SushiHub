37e8c89016f99a57c12df79c3f477967d3329836
owsky <nick.bertocco@gmail.com>
2021-01-01 23:44:07 +0100

    Corretto implementazione factory

--- app/src/main/java/com/veneto_valley/veneto_valley/viewmodel/MyViewModelFactory.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/viewmodel/MyViewModelFactory.java
@@ -3,14 +3,17 @@ package com.veneto_valley.veneto_valley.viewmodel;
 import android.app.Application;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
-public class MyViewModelFactory extends ViewModelProvider.AndroidViewModelFactory {
-	private Application application;
-	private String tavolo;
-	
+import java.lang.reflect.InvocationTargetException;
 
+public class MyViewModelFactory extends ViewModelProvider.AndroidViewModelFactory {
+	private final Application application;
+	private final String tavolo;
+	
+	
 	public MyViewModelFactory(@NonNull Application application, String tavolo) {
 		super(application);
 		this.application = application;
@@ -20,11 +23,19 @@ public class MyViewModelFactory extends ViewModelProvider.AndroidViewModelFactor
 	@NonNull
 	@Override
 	public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-		if (modelClass == PendingViewModel.class)
-			return (T) new PendingViewModel(application, tavolo);
-		else if (modelClass == DeliveredViewModel.class)
-			return (T) new DeliveredViewModel(application, tavolo);
-		else
-			return (T) new ConfirmedViewModel(application, tavolo);
+		if (AndroidViewModel.class.isAssignableFrom(modelClass)) {
+			try {
+				return modelClass.getConstructor(application.getClass(), String.class).newInstance(application, tavolo);
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
+		return super.create(modelClass);
 	}
 }
