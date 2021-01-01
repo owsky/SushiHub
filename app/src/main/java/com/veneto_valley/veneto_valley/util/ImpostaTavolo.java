6cc381d5bcdb35c6a54eb5833f6334af38a11e63
owsky <nick.bertocco@gmail.com>
2021-01-01 23:55:18 +0100

    Rifatto creazione tavolo

--- app/src/main/java/com/veneto_valley/veneto_valley/util/ImpostaTavolo.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/util/ImpostaTavolo.java
@@ -1,25 +1,22 @@
 package com.veneto_valley.veneto_valley.util;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import android.preference.PreferenceManager;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
 import com.veneto_valley.veneto_valley.R;
-import com.veneto_valley.veneto_valley.model.AppDatabase;
-import com.veneto_valley.veneto_valley.model.entities.Tavolo;
+import com.veneto_valley.veneto_valley.viewmodel.CreaTavoloViewModel;
 
 public class ImpostaTavolo extends Fragment {
-	
+	//TODO inserire nome ristorante
 	public ImpostaTavolo() {
 		super(R.layout.fragment_imposta_tavolo);
 	}
@@ -33,22 +30,12 @@ public class ImpostaTavolo extends Fragment {
 		
 		finito.setOnClickListener(v -> {
 			if (TextUtils.isEmpty(costoMenu.getText()))
-				costoMenu.setError("Inserisci il costo del menu");
-			else if (TextUtils.isEmpty(portate.getText()))
-				portate.setError("Inserisci il numero massimo di portate");
+				Toast.makeText(requireContext(), "Inserisci il costo del menu", Toast.LENGTH_SHORT).show();
 			else {
-				SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext());
-				SharedPreferences.Editor editor = sharedPreferences.edit();
-				
-				ImpostaTavoloArgs args;
-				if (getArguments() != null && (args = ImpostaTavoloArgs.fromBundle(getArguments())).getCodiceTavolo() != null) {
-					editor.putString("codice_tavolo", args.getCodiceTavolo());
-					editor.putBoolean("is_master", true);
-					editor.apply();
-					Tavolo tavolo = new Tavolo(args.getCodiceTavolo(), Integer.parseInt(portate.getText().toString()), Float.parseFloat(costoMenu.getText().toString()));
-					AppDatabase.getInstance(requireContext()).tavoloDao().insertAll(tavolo);
-				}
-				NavHostFragment.findNavController(ImpostaTavolo.this).navigate(R.id.action_impostaTavolo_to_listaPiattiFragment);
+				ImpostaTavoloArgs args = ImpostaTavoloArgs.fromBundle(requireArguments());
+				CreaTavoloViewModel viewModel = ViewModelUtil.getViewModel(requireActivity(), CreaTavoloViewModel.class);
+				viewModel.creaTavolo(args.getCodiceTavolo(), Integer.parseInt(portate.getText().toString()), Float.parseFloat(costoMenu.getText().toString()));
+				NavHostFragment.findNavController(this).navigate(R.id.action_impostaTavolo_to_listaPiattiFragment);
 			}
 		});
 	}
---
+++ app/src/main/java/com/veneto_valley/veneto_valley/viewmodel/CreaTavoloViewModel.java
@@ -0,0 +1,26 @@
+package com.veneto_valley.veneto_valley.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.veneto_valley.veneto_valley.util.RepositoryTavoli;
+
+public class CreaTavoloViewModel extends AndroidViewModel {
+	private final RepositoryTavoli repository;
+	
+	public CreaTavoloViewModel(@NonNull Application application) {
+		super(application);
+		repository = new RepositoryTavoli(application);
+	}
+	
+	//TODO: se sei slave ricevi tavolo da master
+	public void creaTavolo(String codice) {
+		repository.creaTavolo(codice);
+	}
+	
+	public void creaTavolo(String codice, int portate, float menu) {
+		repository.creaTavolo(codice, portate, menu);
+	}
+}
