a48cfcd6afd7963d38a567dd971219890203af65
owsky <nick.bertocco@gmail.com>
2021-01-02 16:30:46 +0100

    Implementato switch extra

--- app/src/main/java/com/veneto_valley/veneto_valley/view/UserInputPage.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/view/UserInputPage.java
@@ -3,7 +3,9 @@ package com.veneto_valley.veneto_valley.view;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
@@ -11,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.veneto_valley.veneto_valley.R;
 import com.veneto_valley.veneto_valley.model.entities.Ordine;
 import com.veneto_valley.veneto_valley.util.ViewModelUtil;
@@ -18,6 +21,8 @@ import com.veneto_valley.veneto_valley.viewmodel.PendingViewModel;
 
 public class UserInputPage extends Fragment {
 	private EditText codice, desc, qta, prezzo;
+	private TextView prezzoTextView;
+	private boolean isExtra;
 	
 	public UserInputPage() {
 		super(R.layout.fragment_user_input);
@@ -42,8 +47,11 @@ public class UserInputPage extends Fragment {
 		desc = view.findViewById(R.id.addDesc);
 		qta = view.findViewById(R.id.addQuantita);
 		prezzo = view.findViewById(R.id.addPrezzo);
+		prezzoTextView = view.findViewById(R.id.prezzoTextView);
 		Button salvaEsci = view.findViewById(R.id.salvaEsci);
 		Button salvaNuovo = view.findViewById(R.id.salvaNuovo);
+		SwitchMaterial switchMaterial = view.findViewById(R.id.switchExtra);
+		switchMaterial.setOnCheckedChangeListener((buttonView, isChecked) -> flipExtra());
 		
 		UserInputPageArgs args = UserInputPageArgs.fromBundle(requireArguments());
 		Ordine ordine;
@@ -58,6 +66,11 @@ public class UserInputPage extends Fragment {
 				salvaOrdine();
 				NavHostFragment.findNavController(UserInputPage.this).navigateUp();
 			});
+			if (ordine.prezzo > 0) {
+				prezzo.setVisibility(View.VISIBLE);
+				prezzoTextView.setVisibility(View.VISIBLE);
+				switchMaterial.setChecked(true);
+			}
 		} else {
 			salvaEsci.setOnClickListener(v -> {
 				salvaOrdine();
@@ -91,6 +104,21 @@ public class UserInputPage extends Fragment {
 		if (!prezzoExtra.isEmpty())
 			ordine.prezzo = Float.parseFloat(prezzoExtra);
 		
+		if (!isExtra)
+			ordine.prezzo = 0;
+		
 		viewModel.insert(ordine);
+	}
+	
+	private void flipExtra() {
+		if (isExtra) {
+			prezzoTextView.setVisibility(View.INVISIBLE);
+			prezzo.setVisibility(View.INVISIBLE);
+			isExtra = false;
+		} else {
+			prezzoTextView.setVisibility(View.VISIBLE);
+			prezzo.setVisibility(View.VISIBLE);
+			isExtra = true;
+		}
 	}
 }
