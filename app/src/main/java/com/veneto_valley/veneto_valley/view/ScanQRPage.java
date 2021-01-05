0bd7ad0cea2b28488eeaead1f6cdaa2cb9c6e52e
owsky <nick.bertocco@gmail.com>
2021-01-05 16:10:09 +0100

    Rimosso metodo deprecato e implementato activity result launcher

--- app/src/main/java/com/veneto_valley/veneto_valley/view/ScanQRPage.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/view/ScanQRPage.java
@@ -2,50 +2,47 @@ package com.veneto_valley.veneto_valley.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-import com.journeyapps.barcodescanner.CaptureActivity;
 import com.veneto_valley.veneto_valley.R;
 import com.veneto_valley.veneto_valley.util.ViewModelUtil;
 import com.veneto_valley.veneto_valley.viewmodel.CreaTavoloViewModel;
 
+import static com.google.zxing.integration.android.IntentIntegrator.REQUEST_CODE;
+
 public class ScanQRPage extends Fragment {
+	
+	private final ActivityResultLauncher<Intent> scanIntent = registerForActivityResult(
+			new ActivityResultContracts.StartActivityForResult(), result -> {
+				IntentResult res = IntentIntegrator.parseActivityResult(REQUEST_CODE, result.getResultCode(), result.getData());
+				String contents = res.getContents();
+				// se la lettura è andata a buon fine splitta la stringa in array di stringhe per creare il tavolo tramite viewmodel
+				if (contents != null) {
+					String[] info = TextUtils.split(contents, ";");
+					ViewModelUtil.getViewModel(requireActivity(), CreaTavoloViewModel.class).creaTavolo(info[0], Integer.parseInt(info[1]), Float.parseFloat(info[2]));
+					NavHostFragment.findNavController(ScanQRPage.this).navigate(R.id.action_scanQRNav_to_impostaUtentePage);
+				} else {
+					NavHostFragment.findNavController(ScanQRPage.this).navigateUp();
+				}
+			}
+	);
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		scanCode();
-	}
-	
-	private void scanCode() {
-		// impostazioni activity zxing scanner e avvio
+		// impostazioni zxing scanner e avvio
 		IntentIntegrator integrator = IntentIntegrator.forSupportFragment(this);
 		integrator.setOrientationLocked(true);
 		integrator.setBeepEnabled(false);
 		integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
 		integrator.setPrompt("Scanning QR Code");
-		integrator.initiateScan();
-	}
-	
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-		if (result != null) {
-			String contents = result.getContents();
-			if (contents != null) {
-				// se la lettura è andata a buon fine splitta la stringa in array di stringhe per creare il tavolo tramite viewmodel
-				String[] info = contents.split(";");
-				ViewModelUtil.getViewModel(requireActivity(), CreaTavoloViewModel.class).creaTavolo(info[0], Integer.parseInt(info[1]), Float.parseFloat(info[2]));
-				NavHostFragment.findNavController(this).navigate(R.id.action_scanQRNav_to_impostaUtentePage);
-			} else {
-				NavHostFragment.findNavController(this).navigateUp();
-			}
-		} else {
-			super.onActivityResult(requestCode, resultCode, data);
-		}
+		scanIntent.launch(integrator.createScanIntent());
 	}
 }
