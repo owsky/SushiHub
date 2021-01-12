48ee9cbd33549fd687557ebc57bc6b2c90bc0c64
owsky <nick.bertocco@gmail.com>
2021-01-12 17:46:21 +0100

    Implementato Parcelable

--- app/src/main/java/com/veneto_valley/veneto_valley/model/entities/Ristorante.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/model/entities/Ristorante.java
@@ -1,5 +1,8 @@
 package com.veneto_valley.veneto_valley.model.entities;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.annotation.NonNull;
 
 import com.google.firebase.database.Exclude;
@@ -9,7 +12,7 @@ import java.util.ArrayList;
 
 // Firebase entity
 @IgnoreExtraProperties
-public class Ristorante {
+public class Ristorante implements Parcelable {
     @Exclude
     public String idRistorante;
     public String nome;
@@ -21,9 +24,9 @@ public class Ristorante {
     }
 
     //TODO: Si può eliminare?
-    public Ristorante(String nome) {
-        this.nome = nome;
-    }
+//    public Ristorante(String nome) {
+//        this.nome = nome;
+//    }
 
     public Ristorante(String idRistorante, String nome, String indirizzo, String localita) {
         this.idRistorante = idRistorante;
@@ -44,6 +47,39 @@ public class Ristorante {
     public String toString() {
         return this.nome + " - " + this.localita;
     }
+    
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+    
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(idRistorante);
+        dest.writeString(nome);
+        dest.writeString(indirizzo);
+        dest.writeString(localita);
+    }
+    
+    protected Ristorante(Parcel in) {
+        idRistorante = in.readString();
+        nome = in.readString();
+        indirizzo = in.readString();
+        localita = in.readString();
+    }
+    
+    @Exclude
+    public static final Parcelable.Creator<Ristorante> CREATOR = new Parcelable.Creator<Ristorante>() {
+        @Override
+        public Ristorante createFromParcel(Parcel in) {
+            return new Ristorante(in);
+        }
+        
+        @Override
+        public Ristorante[] newArray(int size) {
+            return new Ristorante[size];
+        }
+    };
 }
 
 
