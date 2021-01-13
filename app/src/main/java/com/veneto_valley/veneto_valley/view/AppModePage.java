e3ff4e544440d1c1415c6d1e47bd6b6c82031825
owsky <nick.bertocco@gmail.com>
2021-01-13 18:37:14 +0100

    Riciclato un layout

--- app/src/main/java/com/veneto_valley/veneto_valley/view/AppModePage.java
+++ app/src/main/java/com/veneto_valley/veneto_valley/view/AppModePage.java
@@ -3,6 +3,7 @@ package com.veneto_valley.veneto_valley.view;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,16 +15,22 @@ import com.veneto_valley.veneto_valley.R;
 public class AppModePage extends Fragment {
 	
 	public AppModePage() {
-		super(R.layout.fragment_app_mode_page);
+		super(R.layout.fragment_homepage);
 	}
 	
 	@Override
 	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
-		Button conv = view.findViewById(R.id.convBtn);
+		TextView greeting = view.findViewById(R.id.greeting);
+		greeting.setText(R.string.conv_greeting);
+		
+		Button conv = view.findViewById(R.id.btnCrea);
+		conv.setText("Ristorante\nConvenzionato");
 		conv.setOnClickListener(v -> NavHostFragment.findNavController(AppModePage.this)
 				.navigate(R.id.action_appModePage_to_listaRistorantiNav));
-		Button nonConv = view.findViewById(R.id.nonConvBtn);
+		
+		Button nonConv = view.findViewById(R.id.btnUnisciti);
+		nonConv.setText("Ristorante non\nConvenzionato");
 		nonConv.setOnClickListener(v -> NavHostFragment.findNavController(AppModePage.this)
 				.navigate(R.id.action_appModeNav_to_impostaTavoloNav));
 	}
